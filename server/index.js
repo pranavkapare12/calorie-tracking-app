26852deda5da2299082cdc71ae9fe68e922d51d0
Pranav Kapare <sarangkapare12345@gmail.com>
2026-07-22 21:32:04 +0530

    Create folder Structure and update the code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import { config } from 'dotenv';
+
 
 const app = express();
-
 app.use(express.json())
+config()
 
 app.get('/',(req,res)=>{
     res.status(200).json({
