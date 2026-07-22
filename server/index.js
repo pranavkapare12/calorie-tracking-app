a8d1120be9a61f8d9da63be3d0b74e52460c79f8
Pranav Kapare <sarangkapare12345@gmail.com>
2026-07-22 22:53:19 +0530

    REMOVE CORS ERROE

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import { config } from 'dotenv';
-
 import auth_route from './server_modules/routes/auth.routes.js';
+import cors from "cors"
+
+config()
 
 const app = express();
+app.use(cors({
+    "origin":"*",
+    optionsSuccessStatus: 200  
+}))
 app.use(express.json())
-config()
 
 app.get('/',(req,res)=>{
     res.status(200).json({
