09be22df46f522c48bcca1352869552a3e4e8c2d
Pranav Kapare <sarangkapare12345@gmail.com>
2026-07-22 22:39:20 +0530

    Fix Auth Bugs

--- server/server_modules/controler/auth.controler.js
+++ server/server_modules/controler/auth.controler.js
@@ -1,85 +1,70 @@
-import { hashPassword,compareHash } from "../Functions/crypto.functions";
+import { hashPassword,compareHash } from "../Functions/crypto.functions.js";
 import { compareSync } from "bcrypt";
-
+import { generateToken } from "../Functions/token.function.js";
+import cookieParser from "cookie-parser";
 const loging = (req, res) => {
     const { email, password } = req.body;
-    let conn = mongoDb();
-    const userDbResult = await User.findOne({
-        email: email
-    })
+   
+    // let getCookie = generateToken(userDbResult._id);
+    // const userData = {
+    //     _id: userDbResult._id,
+    //     username: userDbResult.username,
+    //     email: userDbResult.email,
+    //     type: userDbResult.type,
+    //     createAt: userDbResult.createdAt
+    // }
+    // res.cookie('Grocery_User', getCookie, {
+    //     httpOnly: true,
+    //     secure: true,
+    //     sameSite: "none",
+    //     path: "/",
+    //     maxAge: 7 * 24 * 60 * 60 * 1000
+    // })
+    // let userDate = "Hello From the user"
 
-    if (!userDbResult) {
-        return res.status(400).json({
-            message: "INVALID USER NAME OR PASSWORD"
-        })
-    }
-
-    let result = await compareHash(password, userDbResult.password);
-    if (!result) {
-        return res.status(400).json({
-            message: "INVALID USER NAME OR PASSWORD"
-        })
-    }
-
-
-    let getCookie = generateToken(userDbResult._id);
-    const userData = {
-        _id: userDbResult._id,
-        username: userDbResult.username,
-        email: userDbResult.email,
-        type: userDbResult.type,
-        createAt: userDbResult.createdAt
-    }
-    res.cookie('Grocery_User', getCookie, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        path: "/",
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    })
-
-    req.userData = userData;
-    return res.status(201).json(userData)
+    // req.userData = userData;
+    return res.status(201).json({"message":"All Is Working Correctly"})
 }
 
 const signup = (req, res) => {
     const { username, email, password, type } = req.body;
-    let conn = mongoDb();
-    let ack = await User.findOne({
-        email: email
-    })
+    // let conn = mongoDb();
+    // let ack = await User.findOne({
+    //     email: email
+    // })
 
-    if (ack) {
-        return res.status(409).json({
-            message: "USER ALREADY EXIST"
-        })
-    }
-    let hash = await hashPassword(password);
+    // if (ack) {
+    //     return res.status(409).json({
+    //         message: "USER ALREADY EXIST"
+    //     })
+    // }
+    // let hash = await hashPassword(password);
 
-    let result = await User.create({
-        username: username,
-        email: email,
-        password: hash,
-        type: type
-    })
+    // let result = await User.create({
+    //     username: username,
+    //     email: email,
+    //     password: hash,
+    //     type: type
+    // })
 
 
-    let getToken = generateToken(result._id);
-    const userData = {
-        _id: result._id,
-        username: result.username,
-        email: result.email,
-        type: result.type,
-        createAt: result.createdAt
-    }
-    res.cookie('Grocery_User', getToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        path: "/",
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    });
-    return res.status(201).json(userData);
+    // let getToken = generateToken(result._id);
+    // const userData = {
+    //     _id: result._id,
+    //     username: result.username,
+    //     email: result.email,
+    //     type: result.type,
+    //     createAt: result.createdAt
+    // }
+    // res.cookie('Grocery_User', getToken, {
+    //     httpOnly: true,
+    //     secure: true,
+    //     sameSite: "none",
+    //     path: "/",
+    //     maxAge: 7 * 24 * 60 * 60 * 1000
+    // });
+
+    return res.status(201).json({"message":"Sign Up is Working Correctly"});
 }
 
 const logout = (req, res) => {
