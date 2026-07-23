c34d6ae350c59054f3d7a4e410a988a9f7c412bc
Pranav Kapare <sarangkapare12345@gmail.com>
2026-07-23 09:58:29 +0530

    POSTGRESS CONNECTION CREATED

--- server/server_modules/database_connection/Pg.database_connection.js
+++ server/server_modules/database_connection/Pg.database_connection.js
@@ -1,7 +1,14 @@
 import pg from "pg"
-
+import dotenv from "dotenv"
+dotenv.config()
+let pv = process.env;
+// console.log(pv.IN_PROCESS == "TRUE" ? pv.D_USER : pv.P_USER)
 let pool =new pg.Pool({
-    
+    user: pv.IN_PROCESS == "TRUE" ? pv.D_USER : pv.P_USER,
+    host: pv.IN_PROCESS == "TRUE" ? pv.D_HOST : pv.P_HOST,
+    database : pv.IN_PROCESS == "TRUE" ? pv.D_DATABASE : pv.P_DATABASE,
+    password : pv.IN_PROCESS == "TRUE" ? pv.D_PASSWORD : pv.P_PASSWORD,
+    port : parseInt(pv.IN_PROCESS == "TRUE" ? pv.D_PORT : pv.P_PORT)
 })
 
 export default pool;
